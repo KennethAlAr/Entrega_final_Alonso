9140069dbd807e1b59af6cf09703418d0c8d704c
KennethAlAr <kennethalonsoarce@gmail.com>
2024-04-28 21:57:21 +0200

    interfaz usuarios

--- partidas/urls.py
+++ partidas/urls.py
@@ -7,13 +7,21 @@ from .views import (
     JuegoDeleteView,
     JuegoUpdateView,
     JuegoCreateView,
+    user_login_view,
+    user_logout_view,
+    UserUpdateView,
+    user_creation_view,
 )
 
 urlpatterns = [
-    path("", home_view),
+    path("", home_view, name="home"),
     path("juego/list/", JuegoListView.as_view(), name="juego-list"),
     path("juego/create/", JuegoCreateView.as_view(), name="juego-create"),
     path("juego/<int:pk>/detail/", JuegoDetailView.as_view(), name="juego-detail"),
     path("juego/<int:pk>/update/", JuegoUpdateView.as_view(), name="juego-update"),
     path("juego/<int:pk>/delete/", JuegoDeleteView.as_view(), name="juego-delete"),
+    path("login/", user_login_view, name='login'),
+    path("logout/", user_logout_view, name='logout'),
+    path("editar-perfil/", UserUpdateView.as_view(), name='editar-perfil'),
+    path("crear-usuario/", user_creation_view, name='crear-usuario'),
 ]
--- partidas/views.py
+++ partidas/views.py
@@ -1,3 +1,6 @@
+from .models import Juego, Reserva, Sistema
+from .forms import UserEditForm
+from django import forms
 from django.urls import reverse_lazy
 from django.views.generic import(
     ListView,
@@ -6,10 +9,11 @@ from django.views.generic import(
     UpdateView,
     DeleteView,
 )
-
 from django.shortcuts import render, redirect
-
-from .models import Juego, Reserva, Sistema
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -18,22 +22,22 @@ def home_view(request):
 
 #CRUD JUEGOS
 
-class JuegoListView(ListView):
+class JuegoListView(LoginRequiredMixin, ListView):
     model = Juego
     template_name = "partidas/vbc/juego-list.html"
     context_object_name = "todos_los_juegos"
 
-class JuegoDetailView(DetailView):
+class JuegoDetailView(LoginRequiredMixin, DetailView):
     model = Juego
     template_name = "partidas/vbc/juego-detail.html"
     context_object_name = "juego"
 
-class JuegoDeleteView(DeleteView):
+class JuegoDeleteView(LoginRequiredMixin, DeleteView):
     model = Juego
     template_name = "partidas/vbc/juego-confirm-delete.html"
     success_url = reverse_lazy("juego-list")
 
-class JuegoUpdateView(UpdateView):
+class JuegoUpdateView(LoginRequiredMixin, UpdateView):
     model = Juego
     template_name = "partidas/vbc/juego-form.html"
     fields = [
@@ -46,13 +50,77 @@ class JuegoUpdateView(UpdateView):
         "hora_inicio",
         "hora_fin"
         ]
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['fecha'].widget = forms.DateInput(attrs={'type': 'date'})
+        form.fields['hora_inicio'].widget = forms.TimeInput(attrs={'type': 'time'})
+        form.fields['hora_fin'].widget = forms.TimeInput(attrs={'type': 'time'})
+        return form
+
     context_object_name = "juego"
     success_url = reverse_lazy("juego-list")
 
-class JuegoCreateView(CreateView):
+class JuegoCreateView(LoginRequiredMixin, CreateView):
     model = Juego
     template_name = "partidas/vbc/juego-form.html"
-    fields = ["nombre", "disponible", "numero_de_jugadores", "sistema_de_juego", "descripcion", "fecha", "hora_inicio", "hora_fin"]
+    fields = [
+        "nombre",
+        "disponible",
+        "numero_de_jugadores",
+        "sistema_de_juego",
+        "descripcion",
+        "fecha",
+        "hora_inicio",
+        "hora_fin"
+        ]
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['fecha'].widget = forms.DateInput(attrs={'type': 'date'})
+        form.fields['hora_inicio'].widget = forms.TimeInput(attrs={'type': 'time'})
+        form.fields['hora_fin'].widget = forms.TimeInput(attrs={'type': 'time'})
+        return form
+    
     success_url = reverse_lazy("juego-list")
 
-# Create 
+# login / logout / Editar usuario / Crear Usuario 1:50
+
+def user_login_view(request):
+    if request.method == "GET":
+        form = AuthenticationForm()
+    elif request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.user_cache
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+    
+    return render(request, "partidas/login.html", {"ingresar": form})
+
+def user_logout_view(request):
+    logout(request)
+    return redirect("login")
+
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = UserEditForm
+    template_name = "partidas/user-edit-form.html"
+    success_url = reverse_lazy("home")
+
+    def get_object(self):
+        return self.request.user
+
+def user_creation_view(request):
+    if request.method == "GET":
+        form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    
+    return render(request, "partidas/crear-usuario.html", {"form": form})
