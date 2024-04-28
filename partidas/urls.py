27cad8552f814670e579bb761a02c991e96af8bf
KennethAlAr <kennethalonsoarce@gmail.com>
2024-04-28 20:36:31 +0200

    CRUD juegos

--- partidas/urls.py
+++ partidas/urls.py
@@ -2,8 +2,18 @@ from django.urls import path
 
 from .views import (
     home_view,
+    JuegoListView,
+    JuegoDetailView,
+    JuegoDeleteView,
+    JuegoUpdateView,
+    JuegoCreateView,
 )
 
 urlpatterns = [
     path("", home_view),
+    path("juego/list/", JuegoListView.as_view(), name="juego-list"),
+    path("juego/create/", JuegoCreateView.as_view(), name="juego-create"),
+    path("juego/<int:pk>/detail/", JuegoDetailView.as_view(), name="juego-detail"),
+    path("juego/<int:pk>/update/", JuegoUpdateView.as_view(), name="juego-update"),
+    path("juego/<int:pk>/delete/", JuegoDeleteView.as_view(), name="juego-delete"),
 ]
--- partidas/views.py
+++ partidas/views.py
@@ -1,14 +1,58 @@
+from django.urls import reverse_lazy
+from django.views.generic import(
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+
 from django.shortcuts import render, redirect
+
+from .models import Juego, Reserva, Sistema
 
 # Create your views here.
 
 def home_view(request):
     return render(request, "partidas/home.html")
 
-#CRUD SISTEMAS
+#CRUD JUEGOS
 
-# List
-# Detail
-# Update
+class JuegoListView(ListView):
+    model = Juego
+    template_name = "partidas/vbc/juego-list.html"
+    context_object_name = "todos_los_juegos"
+
+class JuegoDetailView(DetailView):
+    model = Juego
+    template_name = "partidas/vbc/juego-detail.html"
+    context_object_name = "juego"
+
+class JuegoDeleteView(DeleteView):
+    model = Juego
+    template_name = "partidas/vbc/juego-confirm-delete.html"
+    success_url = reverse_lazy("juego-list")
+
+class JuegoUpdateView(UpdateView):
+    model = Juego
+    template_name = "partidas/vbc/juego-form.html"
+    fields = [
+        "nombre",
+        "disponible",
+        "numero_de_jugadores",
+        "sistema_de_juego",
+        "descripcion",
+        "fecha",
+        "hora_inicio",
+        "hora_fin"
+        ]
+    context_object_name = "juego"
+    success_url = reverse_lazy("juego-list")
+
+class JuegoCreateView(CreateView):
+    model = Juego
+    template_name = "partidas/vbc/juego-form.html"
+    fields = ["nombre", "disponible", "numero_de_jugadores", "sistema_de_juego", "descripcion", "fecha", "hora_inicio", "hora_fin"]
+    success_url = reverse_lazy("juego-list")
+
 # Create 
-# Delete
